805e8584c646efe5a597f700b11c8e1ee05047ca
Abhishek <2003abhisheksk@gmail.com>
2024-12-02 20:01:17 +0530

    Database file added

---
+++ Database/registration.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Registration (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    dob DATE NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
